fa21c04b774480f3b66029a8e2a0c903edd52541
Rares Golea <rgolea@users.noreply.github.com>
2023-01-04 21:27:57 +0100

    Initial commit

---
+++ apps/ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'file-upload-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui';
+}
